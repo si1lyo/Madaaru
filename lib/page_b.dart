25afa10114be519c08af170c5dfaae8c0f5fc2b4
ihuhas <peteryamagami@gmail.com>
2026-06-03 18:47:20 +0900

    カレンダー表示

--- lib/page_b.dart
+++ lib/page_b.dart
@@ -1,14 +1,57 @@
 import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
 
-class PageB extends StatelessWidget {
+class PageB extends StatefulWidget {
   const PageB({super.key});
+
+  @override
+  State<PageB> createState() => _PageBState();
+}
+
+class _PageBState extends State<PageB> {
+  DateTime _focusedDay = DateTime.now();
+  DateTime? _selectedDay;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Page B')),
-      body: const Center(
-        child: Text('ここは担当者Bのページ'),
+      appBar: AppBar(
+        title: const Text('カレンダー'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: TableCalendar(
+          firstDay: DateTime.utc(2020, 1, 1),
+          lastDay: DateTime.utc(2035, 12, 31),
+          focusedDay: _focusedDay,
+
+          selectedDayPredicate: (day) {
+            return isSameDay(_selectedDay, day);
+          },
+
+          onDaySelected: (selectedDay, focusedDay) {
+            setState(() {
+              _selectedDay = selectedDay;
+              _focusedDay = focusedDay;
+            });
+          },
+
+          headerStyle: const HeaderStyle(
+            titleCentered: true,
+            formatButtonVisible: false,
+          ),
+
+          calendarStyle: const CalendarStyle(
+            todayDecoration: BoxDecoration(
+              color: Colors.orange,
+              shape: BoxShape.circle,
+            ),
+            selectedDecoration: BoxDecoration(
+              color: Colors.blue,
+              shape: BoxShape.circle,
+            ),
+          ),
+        ),
       ),
     );
   }
